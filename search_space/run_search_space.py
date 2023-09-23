ba10a9fa45be1ee342792624290fbd52d83ab3eb
Mitchell <35874916+cookem4@users.noreply.github.com>
2023-09-22 21:30:57 -0400

    Update run_search_space.py

--- search_space/run_search_space.py
+++ search_space/run_search_space.py
@@ -1,6 +1,21 @@
 
 import subprocess
 
+# Data container
+class Ntt_Data:
+    def __init__(self, type_str):
+        self.type_str = type_str
+        self.dim_data_x = []
+        self.runtime_y = []
+        self.heap_y = []
+        self.ir_cnt_y = []
+        self.stack_height_y = []
+        self.code_size_B_y = []
+        self.code_size_instr_y = []
+        self.prog_output = []
+        self.exec_status_y = [] # Pass/fail
+
+
 def run_bash_cmd(bash_command):
     try:
         # Run the command and capture the output and error
@@ -23,27 +38,78 @@ def main():
     DIM_LST = small_range
     NTT_TYPE_LST = ["TYPE_MTX", "TYPE_N2_1", "TYPE_N2_2", "TYPE_N2_3", "TYPE_FAST_FIXED", "TYPE_FAST_MIXED", "TYPE_FAST_FIXED_INPLACE", "TYPE_FAST_MIXED_INPLACE"]
     bash_command_base = "make clean && make CFLAGS=\"-O2"
+
+    ntt_type_objs = []
+    for ntt_type in NTT_TYPE_LST:
+        new_data_obj = Ntt_Data(ntt_type)
+        ntt_type_objs.append(new_data_obj)
+
     for dim in DIM_LST:
-        for ntt_type in NTT_TYPE_LST:
-            bash_command = bash_command_base + " -DDIM=" + str(dim) + " -DNTT_TYPE=" + ntt_type
+        for ntt_type in ntt_type_objs:
+            ntt_type.dim_x.append(dim)
+            bash_command_build = bash_command_base + " -DDIM=" + str(dim) + " -DNTT_TYPE=" + ntt_type.type_str
             # TODO add this later
             '''
-            if ntt_type == "TYPE_FAST_FIXED" or ntt_type == "TYPE_FAST_MIXED":
+            if ntt_type.type_str == "TYPE_FAST_FIXED" or ntt_type.type_str == "TYPE_FAST_MIXED":
                 bash_command = bash_command + " " + CHECK_STACK
             '''
-            bash_command = bash_command + "\""
+            bash_command = bash_command_build + "\""
             # Compile the program
             output = run_bash_cmd(bash_command)
             # Run the program
             output = run_bash_cmd(PROG_NAME)
+            ntt_type.prog_output.append(output)
             if (output.find("PASS") != -1):
-                print("N = " + str(dim) + " PASSED for NTT: " + ntt_type)
+                ntt_type.exec_status_y.append("PASS")
+                print("N = " + str(dim) + " PASSED for NTT: " + ntt_type.type_str)
             else:
-                print("N = " + str(dim) + " FAILED for NTT: " + ntt_type)
-    # Tools:
-    # valgrind --tool=cachegrind,massif,callgrind
-    # Can then run callgrind_annotate for the given callgrind log
-    # Stack profiling for recursive methods only: define CHECK_STACK
+                ntt_type.exec_status_y.append("FAIL")
+                print("N = " + str(dim) + " FAILED for NTT: " + ntt_type.type_str)
+
+            # Tools:
+            # valgrind --tool=cachegrind,massif,callgrind
+            # Can then run callgrind_annotate or ms_print for the given callgrind log
+            # Stack profiling for recursive methods only: define CHECK_STACK
+
+            bash_command = "valgrind --tool=massif " + PROG_NAME
+            output = run_bash_cmd(bash_command)
+            # Parse the massif output
+            bash_command = "ls massif* | ms_print"
+            output = run_bash_command(bash_command)
+            # TODO parse the ms_print output and assign to obj
+            bash_command = "rm massif*"
+            output = run_bash_command(bash_command)
+
+            bash_command = "valgrind --tool=callgrind " + PROG_NAME
+            output = run_bash_cmd(bash_command)
+            # Parse the massif output
+            bash_command = "ls callgrind* | callgrind_annotate"
+            output = run_bash_command(bash_command)
+            # TODO parse the callgrind anotate output and assign to obj
+            bash_command = "rm callgrind*"
+            output = run_bash_command(bash_command)
+
+            # Check code size in B
+            bash_command = "size ntt.o"
+            output = run_bash_command(bash_command)
+            # TODO parse output
+
+            # Check code size in Assembly lines
+            bash_command = "objdump -d -t ntt.o | awk '/ntt_impl$/,/^$/' | wc -l"
+            output = run_bash_command(bash_command)
+            # TODO parse output
+
+            # Check runtime with flag
+            bash_command_build = bash_command_build + " -DDO_TIME=1 \""
+            # Compile with timing
+            output = run_bash_cmd(bash_command)
+            # Execute
+            output = run_bash_cmd(PROG_NAME)
+            # TODO parse output
+
+    # Print as CSV
+    headers = ["Dim", "Status", "Runtime", "Heap", "IR Cnt", "Code Size B", "Code size", "Instr"]
+
 
 if __name__ == "__main__":
     main()
